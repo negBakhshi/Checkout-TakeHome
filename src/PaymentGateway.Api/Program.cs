44d5b4658cbab2107829af9e069b94240c31ac11
Negar <n.bakhshi@live.com>
2026-09-16 11:03:34 +0100

    [Negar-004] Enhance Program.cs with HTTP client configuration and add validation for payment requests

--- src/PaymentGateway.Api/Program.cs
+++ src/PaymentGateway.Api/Program.cs
@@ -1,15 +1,36 @@
+using System.Text.Json.Serialization;
+using FluentValidation;
+using PaymentGateway.Api.BankSimulator;
+using PaymentGateway.Api.Models.Requests;
 using PaymentGateway.Api.Services;
+using PaymentGateway.Api.Validation;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddSingleton<PaymentsRepository>();
+builder.Services.AddHttpClient<IBankClient, BankClient>(client =>
+{
+    client.BaseAddress = new Uri(builder.Configuration["BankSimulator:BaseUrl"]!);
+    client.Timeout = TimeSpan.FromSeconds(10);
+});
+
+builder.Services.AddSingleton<IPaymentsRepository, PaymentsRepository>();
+builder.Services.AddSingleton(TimeProvider.System);
+
+builder.Services.AddScoped<IValidator<PostPaymentRequest>, PostPaymentRequestValidator>();
+builder.Services.AddScoped<IPaymentService, PaymentService>();
+
+builder.Services.AddProblemDetails();
+builder
+    .Services.AddControllers()
+    .AddJsonOptions(o => o.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter()));
 
 var app = builder.Build();
 
@@ -27,3 +48,7 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
+// Makes the implicit Program class visible to WebApplicationFactory<Program> in the test project.
+public partial class Program { }
+
