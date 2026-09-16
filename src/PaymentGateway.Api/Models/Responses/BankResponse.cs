66a7e13a57c0856f5f6b870a7acc8a7fdde3eced
Negar <n.bakhshi@live.com>
2026-09-16 10:54:35 +0100

    [Negar-001] Add payment request and response models with updated properties

---
+++ src/PaymentGateway.Api/Models/Responses/BankResponse.cs
@@ -0,0 +1,11 @@
+using System.Text.Json.Serialization;
+
+public record BankResponse
+{
+    [JsonPropertyName("authorized")]
+    public bool Authorized { get; init; }
+
+    [JsonPropertyName("authorization_code")]
+    public string? AuthorizationCode { get; init; }
+}
+
--- src/PaymentGateway.Api/Models/Responses/PostPaymentResponse.cs
+++ src/PaymentGateway.Api/Models/Responses/PostPaymentResponse.cs
@@ -1,12 +1,15 @@
-﻿namespace PaymentGateway.Api.Models.Responses;
+﻿using PaymentGateway.Api.Enums;
+
+namespace PaymentGateway.Api.Models.Responses;
 
 public class PostPaymentResponse
 {
     public Guid Id { get; set; }
     public PaymentStatus Status { get; set; }
-    public int CardNumberLastFour { get; set; }
+    public required string CardNumberLastFour { get; set; }
     public int ExpiryMonth { get; set; }
     public int ExpiryYear { get; set; }
-    public string Currency { get; set; }
+    public required string Currency { get; set; }
     public int Amount { get; set; }
 }
+
